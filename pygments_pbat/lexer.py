2c139f5cec5ef24d0a5a42adc72464b326f32162
Simon Hochrein <10032418+simonhochrein@users.noreply.github.com>
2025-07-10 12:30:36 -0500

    Ah. That was simple

--- pygments_pbat/lexer.py
+++ pygments_pbat/lexer.py
@@ -9,9 +9,9 @@ class PBatLexer(RegexLexer):
 
     tokens = {
         'root': [
-            (r'\$[a-zA-Z0-9_.]+\$', Name.Variable),
             (r'SET|UNSET|SAVEVARS|ECHO|MESSAGE|ADDWIDGET|CLEARWIDGETS|SETTITLE|EVAL|GOTO|IF|ELSE|ELSEIF|ENDIF|COPY|RM|RRM|FORMAT|MKDIR|REDIRFILE|UNMOUNT|FPRINT|LOADEXEC|KEEP|RETURN|EXIT|KILLSESS|SKIPBACK|LOADIMG|UNLOADIMG|PARSEPATH|SETAUTH|SETBIOS|SHUTDOWN|PEEKB|PEEKH|PEEKW|POKEB|POKEH|POKEW|GETDISKTYPE|PARSECNF|CYCLETRAY|LOADSRAM|REBOOTIOP', Keyword),
             (r'EQU|NEQ|GTE|GT|LTE|LT|EXISTS|MATCHES|FAIL|MODLOADED|ISIN|NOT', Keyword.Pseudo),
+            (r'\$[a-zA-Z0-9_.]+\$', Name.Variable),
             (r'-?[0-9]+', Number.Integer),
             (r':[a0zA-Z0-9_]+', Name.Function),
             (r'"', String.Double, 'string'),
@@ -21,7 +21,7 @@ class PBatLexer(RegexLexer):
         'string': [
             (r'^"', Literal.String.Escape),
             (r'\$[a-zA-Z0-9_.]+\$', Name.Variable),
-            (r'[^"]+', String.Double),
+            (r'[^"$]+', String.Double),
             (r'"', String.Double, '#pop')
         ]
     }
